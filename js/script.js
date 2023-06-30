ee4d98a74a7ce70e0cc92338aa08929523fd4749
Devika Ravi <devika.ravi@techversantinfotech.com>
2023-06-30 13:05:27 +0530

    Form validation using jQuery method 3.

--- js/script.js
+++ js/script.js
@@ -1,92 +1,69 @@
 $(document).ready(function() {
     $("#formToValidate").submit(function(event) {
         event.preventDefault();
-        validateInputs();
-    });
-
-    function setError(element, message) {
-        const inputControl = $(element).parent();
-        const errorDisplay = inputControl.find(".errorMessage");
-        errorDisplay.html(message);
-        inputControl.addClass("errorStyle").removeClass("successStyle");
-    }
-
-    function setSuccess(element) {
-        const inputControl = $(element).parent();
-        const errorDisplay = inputControl.find(".errorMessage");
-        errorDisplay.html("");
-        inputControl.addClass("successStyle").removeClass("errorStyle");
-    }
-
-    function isValidName(name) {
-        const regEx = /^[a-zA-Z\-]+$/;
-        return regEx.test(name);
-    }
-
-    function isValidEmailID(emailID) {
-        const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return regEx.test(String(emailID).toLowerCase());
-    }
-
-    function isValidNumber(phoneNumber) {
-        const regEx = /^\d{10}$/;
-        return regEx.test(phoneNumber);
-    }
-
-    function validateInputs() {
-        const firstNameValue = $("#firstName").val().trim();
-        const lastNameValue = $("#lastName").val().trim();
-        const emailIDValue = $("#emailID").val().trim();
-        const phoneNumberValue = $("#phoneNumber").val().trim();
-        const passwordValue = $("#password").val().trim();
-
-        //Performing validation for inputs
-
-        if (firstNameValue === "") {
-            setError("#firstName", "First Name is required");
-        } else if (!isValidName(firstNameValue)) {
-            setError("#firstName", "Please provide a valid first name");
-        } else {
-            setSuccess("#firstName");
-        }
         
-        if (lastNameValue === "") {
-            setError("#lastName", "Last Name is required");
-        } else if (!isValidName(lastNameValue)) {
-            setError("#lastName", "Please provide a valid last name");
+        let firstName = $('#firstName').val();
+        let lastName = $('#lastName').val();
+        let password = $('#password').val();
+        let emailID = $('#emailID').val();
+        let phoneNumber = $('#phoneNumber').val();
+        let feedback = $('#feedback').val();
+
+        $(".error").remove();
+            
+        if(firstName.length<1){
+            $('#firstName').after('<div class="error">name is required</div>');
+        }    
+            
+        if(lastName.length<1){
+            $('#lastName').after('<div class="error">name is required</div>');
+        }
+    
+        if(password.length<5){
+            $('#password').after('<div class="error">password should have 8 character</div>');
+        }
+        if (emailID.length < 1) {
+            $('#emailID').after('<div class="error">enter the email</div>');
         } else {
-            setSuccess("#lastName");
+            var regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            var validEmail = regEx.test(emailID);
+            if (!validEmail) {
+                $('#email').after('<div class="error">Enter a valid email</div>');
+            }
+        }
+            
+        if(isNaN(phoneNumber) || phoneNumber.length <10 || phoneNumber.length>10){
+            $('#phoneNumber').after('<div class="error">enter the phone number</div>');
+        }
+            
+        if($('input[name="gender"]:checked').length == 0){
+            $('.radioButtonClass').after('<div class="error">select any</div>');
         }
 
-        if (emailIDValue === "") {
-            setError("#emailID", "Email is required");
-        } else if (!isValidEmailID(emailIDValue)) {
-            setError("#emailID", "Please provide a valid email address");
-        } else {
-            setSuccess("#emailID");
-        }
+        if($('input[name="agreeMessage"]:checked').length == 0){
+            $('.checkboxClass').after('<div class="error">select any</div>');
+        }else{
+        }	
 
-        if (phoneNumberValue === "") {
-            setError("#phoneNumber", "Phone Number is required");
-        } else if (!isValidNumber(phoneNumberValue)) {
-            setError("#phoneNumber", "Please provide a valid phone number");
-        } else {
-            setSuccess("#phoneNumber");
-        }
+        if($('input[name="termsAgreement"]:checked').length == 0){
+            $('#termsAgreement').after('<div class="error">select any</div>');
+        }else{
+        }	
 
-        if (passwordValue === "") {
-            setError("#password", "Password is required");
-        } else if (passwordValue.length < 8) {
-            setError("#password", "Password must be at least 8 characters.");
-        } else {
-            setSuccess("#password");
-        }
+        // if($('input[name="stateInIndia"]:checked').length == 0){
+        //     $('.dropDownClass').after('<div class="error">select any</div>');
+        // }else{
+        // }	
+
+        if(feedback.length < 1){
+            $('#feedback').after('<div class="error">select any</div>');
+        }	
 
         // Form submission if there are no errors
-        if ($(".errorMessage").text() === "") {
+        if ($(".error").text() === "") {
             // Uncomment the line below to submit the form
             // $("#formToValidate").submit();
             alert("Form submitted successfully!");
         }
-    }
+    });
 });
